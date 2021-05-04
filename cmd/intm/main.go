87ee33ef9a10909e10f16571de6b2e6cc56f052f
Vincent Landgraf <vincent@landgrafx.de>
2021-05-04 21:35:57 +0200

    enable switching alg implementation

--- cmd/intm/main.go
+++ cmd/intm/main.go
@@ -8,12 +8,17 @@ import (
 
 	"github.com/threez/intm/internal/adapter/merger"
 	"github.com/threez/intm/internal/adapter/reader"
+	"github.com/threez/intm/internal/port"
 )
 
-var path string
+var (
+	path string
+	alg  string
+)
 
 func main() {
 	flag.StringVar(&path, "path", "examples/test.txt", "file to merge")
+	flag.StringVar(&alg, "alg", "simple", "used algorithm for merging")
 	flag.Parse()
 
 	f, err := os.Open(path)
@@ -23,7 +28,17 @@ func main() {
 	}
 	defer f.Close()
 
-	m := &merger.Noop{}
+	var m port.Merger
+	switch alg {
+	case "noop":
+		m = &merger.Noop{}
+	case "simple":
+		m = merger.NewSimple()
+	default:
+		fmt.Fprintf(os.Stderr, "unknown merge algorithm %q", alg)
+		os.Exit(1)
+	}
+
 	r := reader.NewReader(f)
 	for {
 		i, err := r.Read()
